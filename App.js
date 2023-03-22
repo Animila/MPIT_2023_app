6524b025a7167d4a56e97343b504ff16394892d7
Animila <hristoforovila5@gmail.com>
2023-03-22 15:37:15 +0900

    добавление шрифтов

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+import * as Font from 'expo-font'
+import { Text } from 'react-native'
+Font.loadAsync({
+	'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+	'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+})
+
 export default function App() {
-	return <div></div>
+	return <Text>Тест</Text>
 }
