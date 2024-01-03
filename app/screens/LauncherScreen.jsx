aca3feda6a1357f9e35c4b282d09a4d828dcd636
Animila <hristoforovila5@gmail.com>
2024-01-03 22:46:01 +0900

    исправил

--- app/screens/LauncherScreen.jsx
+++ app/screens/LauncherScreen.jsx
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { useNavigation } from '@react-navigation/native'
 import React, { useEffect, useState } from 'react'
 import { Image, View } from 'react-native'
@@ -10,26 +9,27 @@ const LauncherScreen = () => {
 	const [isAuth, setIsAuth] = useState(false)
 
 	useEffect(() => {
-		const fetchData = async () => {
-			check()
-			const checked = await AsyncStorage.getItem('user')
-			const user = JSON.parse(checked)
+		navigation.replace('Home')
+		// const fetchData = async () => {
+		// 	check()
+		// 	const checked = await AsyncStorage.getItem('user')
+		// 	const user = JSON.parse(checked)
 
-			console.log(user)
-			if (user) {
-				setIsAuth(true)
-			}
-		}
-		fetchData()
+		// 	console.log(user)
+		// 	if (user) {
+		// 		setIsAuth(true)
+		// 	}
+		// }
+		// fetchData()
 	}, [])
 
-	useEffect(() => {
-		if (isAuth) {
-			navigation.replace('Home')
-		} else {
-			navigation.replace('Auth')
-		}
-	}, [isAuth])
+	// useEffect(() => {
+	// 	if (isAuth) {
+	// 		navigation.replace('Home')
+	// 	} else {
+	// 		navigation.replace('Auth')
+	// 	}
+	// }, [isAuth])
 	return (
 		<View className='w-screen h-full flex flex-col justify-center items-center'>
 			<Image
--- app/service/PlaceService.js
+++ app/service/PlaceService.js
@@ -2,11 +2,10 @@ import axios from 'axios'
 
 export const PlaceService = {
 	async getAll() {
-		axios.defaults.headers.common['Authorization'] =
-			'Bearer 2|4fWyyxwCEpyj6FPVfJZQvHXbDJpi33XsZQKMpxDl'
-		const response = await axios.get(
-			'https://hackaton-yakse.ru/api/culture/get'
-		)
+		// axios.defaults.headers.common['Authorization'] =
+		// 	'Bearer 2|4fWyyxwCEpyj6FPVfJZQvHXbDJpi33XsZQKMpxDl'
+		const response = await axios.get('https://hackaton-yakse.ru/api/v1/bases')
+		console.log(response.data)
 		return response.data
 	},
 }
